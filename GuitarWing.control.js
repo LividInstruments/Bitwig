1829220b15c991bbecd652d061483aa5d67dbf98
aumhaa <aumhaa@gmail.com>
2014-03-17 04:22:19 -0700

    GuitarWing   :   a few tweaks and lots of comments.

--- GuitarWing.control.js
+++ GuitarWing.control.js
@@ -71,13 +71,12 @@ function init()
 	
 	//define global stuff
 	initialize_noteInput();     //here we define the port and channel we want to listen to
-	initialize_prototypes();	//this initializes the processes that our framework uses for communication
+	initialize_prototypes();    //this initializes the processes that our framework uses for communication
 	initialize_surface();       //any controlsurface specific init calls should be placed in this function
 
 
-	setup_controls();		//here we define all our controls by assigning their corresponding MIDI event filters
-	//setup_lcd();			//any textual feedback going to the controlsurface should be set up here
-	resetAll();				//turn off everything on the controlsurface
+	setup_controls();           //here we define all our controls by assigning their corresponding MIDI event filters
+	resetAll();                 //turn off everything on the controlsurface
 
 	//the following calls create js subordinates to the Java functions we're hooking into
 	setup_session();
@@ -88,21 +87,22 @@ function init()
 	setup_usermodes();
 	setup_tasks();
 
-	//setup_modes();			//define the differnet modes we'll be using, and assign controls to its functionality
-	setup_notifications();	//define notifications that will be called when certain functionality is triggered
-	setup_fixed_controls();	//any control assignments that aren't mode-dependent should be initialized here
+	//setup_modes();            //define the differnet modes we'll be using, and assign controls to its functionality
+	setup_notifications();      //define notifications that will be called when certain functionality is triggered
+	setup_fixed_controls();     //any control assignments that aren't mode-dependent should be initialized here
 
-	setup_listeners();		//use this to set up any mode-independent global listeners
-	setupTests();
+	setup_listeners();          //use this to set up any mode-independent global listeners
+	setupTests();               //use this block to set up any diagnostic routines
 
 	LOCAL_OFF();
-	//MainModes.change_mode(0, true);
+	//MainModes.change_mode(0, true);     //select the initial mode
 	post('GuitarWing script loaded! ------------------------------------------------');
 	notifier.show_message('GuitarWing Script version ' + VERSION +' loaded.');
 }
 
 function initialize_noteInput()
 {
+	//we need to tell Bitwig to forward events from the actual MIDI port we're connected to
 	noteInput = host.getMidiInPort(0).createNoteInput("GuitarWing", "80????", "90????", "D0????", "E0????");
 	noteInput.setShouldConsumeEvents(false);
 
@@ -110,11 +110,14 @@ function initialize_noteInput()
 
 function initialize_surface()
 {
-
+	//Any configuration messages that need to be sent to the control surface should go here
 }
 
 function setup_controls()
 {
+	//Assign our control objects to their corresponding MIDI ids.
+	//Each control is an instanceof Notifier object, and capable of notifiying multiple other objects
+	//when a message is received by the script matching its id.
 	script['faders'] = [];
 	script['fader_buttons'] = [];
 	for (var i = 0;i < 3; i++)
@@ -144,43 +147,59 @@ function setup_controls()
 
 function setup_session()
 {
+	//setup a session manager object, and pass it the javaObject we defined in init()
 	session = new SessionComponent('Session', 8, 4, trackBank);
 }
 
 function setup_mixer()
 {
+	//setup a mixer object, and pass it some Java dependencies we created in init()\
+	//We define it with a name, the number of tracks, the number of returns
 	mixer = new MixerComponent('Mixer', 8, 4, trackBank, undefined, cursorTrack, masterTrack);
 
 }
 
 function setup_device()
 {
+	//setup a device object, and pass it the javaObject that we created in init()
+	//we define it with the number of parameters/macros, with a maximum number of 8 per DeviceComponent (this is BW limitation).
 	device = new DeviceComponent('Device', 8, cursorDevice);
 }
 
 function setup_transport()
 {
+	//create a transport object
 	transport = new TransportComponent('Transport');
 }
 
 function setup_instrument_control()
 {
+	//setup an adaptive instrument comprised of a sequencer object and several note transposition objects...this is tricky stuff.
+	//in trying to be as adaptive as possible, things get pretty complicated.  The components that make up the adpative instrument 
+	//can be accessed independently without nearly as much fuss, but the AdaptiveInstrument is neccessary to link the two together.
+	//Have a look at the Prototypes for more information.
 	instrument = new AdaptiveInstrumentComponent('Instrument', {'drum':[4, 4, 0, 0], 'keys':[8, 2, 0, 2], 'drumseq':[4, 4, 4, 0], 'keysseq':[8, 2, 0, 0]}, lcd);
 }
 
 function setup_notifications()
 {
+	//we use this block to setup onscreen notifications that will be triggered when defined parameters change.
 	notifier = new NotificationDisplayComponent();
 }
 
 function setup_tasks()
 {
+	//setup a local task server that is called every 100ms.  We can use this to schedule or repeat events with 
+	//multiple arguments
 	tasks = new TaskServer(script, 100);
 }
 
 function setup_modes()
 {
-	
+	//We can define multiple pages containing different control assignments.  Each page has an enter, update, and exit call.
+	//When entering a mode, enter_mode() is called.  Update_mode should be called when a shift key is pressed.
+	//When leaving the current mode, exit_mode() is called.  
+
 	//Page 0:  Send Control and Instrument throughput
 	mainPage = new Page('mainPage');
 	clipPage.enter_mode = function()
@@ -206,13 +225,18 @@ function setup_modes()
 			clipPage.enter_mode();
 		}
 	}
+
+	//Now that we've created a page, we can create a PageStack to contain it.
 	script["MainModes"] = new PageStack(1, "Main Modes");
 	MainModes.add_mode(0, mainPage);
+	
+	//You can statically set controls to change modes by invoking set_mode_buttons()
 	MainModes.set_mode_buttons([]);
 }
 
 function setup_usermodes()
 {
+	//setup a set of controls that will be sent through a User port to BW, where they can be User-mapped.
 	user1Input = host.getMidiInPort(0).createNoteInput("GuitarWingUser1", "80????", "90????", "D0????", "E0????");
 	userbank1 = new UserBankComponent('UserBank1', 4, user1Input);
 	user1Input.setShouldConsumeEvents(false);
@@ -220,6 +244,7 @@ function setup_usermodes()
 
 function setup_fixed_controls()
 {
+	//this is a convenient block to assign control functions that won't change, even when changing modes.
 	device.set_macro_controls([faders[0], faders[1], faders[2], accels[2], fader_buttons[0], fader_buttons[1], fader_buttons[2], pads[4]]);
 	for(var i=0;i<4;i++)
 	{
@@ -230,7 +255,9 @@ function setup_fixed_controls()
 	{
 		userbank1.set_control(4+i, accels[i]);
 	}
-	mixer._selectedstrip._clip_navigator.set_inc_dec_buttons(buttons[0], buttons[1]);
+	//mixer._selectedstrip._clip_navigator.set_inc_dec_buttons(buttons[0], buttons[1]);
+	session._track_up.set_control(buttons[1]);
+	session._track_down.set_control(buttons[0]);
 	transport._stop.set_control(buttons[6]);
 	transport._loop.set_control(buttons[7]);
 	transport._rewind.set_control(buttons[8]);
@@ -240,16 +267,18 @@ function setup_fixed_controls()
 
 function setup_listeners()
 {
-
+	//here we can assign global listeners by creating new notifiers and adding listen methods to them.
 }
 
 function exit()
 {
+	//reset the controls and exit
 	resetAll();
 }
 
 function onMidi(status, data1, data2)
 {
+	//this is a global callback setup in init(), we could concievably place it in Prototypes instead.
 	 //printMidi(status, data1, data2);
 	if (isChannelController(status)&& MIDIChannel(status) == 0)
 	{
@@ -258,13 +287,14 @@ function onMidi(status, data1, data2)
 	}
 	else if (isNoteOn(status) && MIDIChannel(status) == 0)
 	{
-		post('NOTE: ' + status + ' ' + data1 + ' ' + data2);
+		//post('NOTE: ' + status + ' ' + data1 + ' ' + data2);
 		NOTE_OBJECTS[data1].receive(data2);
 	}
 }
 
 function onSysex(data)
 {
+	//this is a global callback setup in init(), we could concievably place it in Prototypes instead.
 	//printSysex(data);
 }
 
